4f1bbdb26fe087a170d176a561479db58c3c0d58
Ahdiani Febriyanti <z5507587@ad.unsw.edu.au>
2024-07-27 16:21:56 +1000

    import fr etl and cha

--- zid_project2_portfolio.py
+++ zid_project2_portfolio.py
@@ -569,10 +569,10 @@ if __name__ == "__main__":
     # # use test return dict and cha df to test functions:
     ret_dict = etl.aj_ret_dict(tickers=['AAPL', 'TSLA', 'aal', 'abbv', 'bac'],
                                start='2010-05-15', end='2010-08-31')
-    # charc = cha.cha_main(ret_dict, 'vol', ['Daily',])
-    # # #
-    # _test_pf_input_sanity_check(charc, 'vol')
-    # _test_df_reshape(charc, 'vol')
-    # _test_stock_sorting(charc, 'vol', 2)
-    # _test_pf_cal(charc, 'vol', 2)
-    # _test_pf_main(charc, 'vol', 2)
+    charc = cha.cha_main(ret_dict, 'vol', ['Daily',])
+    # #
+    _test_pf_input_sanity_check(charc, 'vol')
+    _test_df_reshape(charc, 'vol')
+    _test_stock_sorting(charc, 'vol', 2)
+    _test_pf_cal(charc, 'vol', 2)
+    _test_pf_main(charc, 'vol', 2)
